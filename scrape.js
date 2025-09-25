c3fcd8d90838cac61bae02f7eb6f502a4591d12d
seacvr <134600598+seancvr@users.noreply.github.com>
2025-09-25 16:32:41 -0600

    first commit

---
+++ scrape.js
@@ -0,0 +1,48 @@
+import { parse } from 'node-html-parser'
+import fs from 'node:fs';
+
+const url = 'https://www.appier.com/en/about/privacy-policy/'
+
+async function getPageSource(url) {
+  try {
+    const response = await fetch(url)
+    const data = await response.text()
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+function writeToFile(content, filename) {
+  fs.writeFile(`${filename}.txt`, content, err => {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log('File written successfully')
+    }
+  })
+}
+
+// extract name from url
+function getName(url) {
+  const hostname = new URL(url).hostname
+  return hostname.split('.')[1]
+}
+
+// parse page with node-html-parser
+function textParser(html) {
+  const root = parse(html)
+  // remove script and style tags from the DOM
+  root.querySelectorAll('script,style,iframe').forEach(el => el.remove());
+  // parse innerText and trim whitespace
+  let text = root.innerText.trim();
+  // remove empty lines
+  text = text.split('\n').filter(line => line.trim() !== '').join('\n');
+  // return text
+  return text
+}
+
+const html = await getPageSource(url)
+const sourceText = textParser(html)
+writeToFile(sourceText, getName(url))
+
